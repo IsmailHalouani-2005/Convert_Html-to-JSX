09adb2418c46b9318e9ef4bd377e6e7e6d619f62
IsmailHalouani-2005 <ismail.halouani@gmail.com>
2021-07-13 12:22:40 +0200

    'Succesful-convert'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import imageInSrc from './meLaughing.jpg';
+import myVideo from './mePunching.mp4'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'1 solid black', maxWidth:'100vw'}}>
+        <h1 className="titleRed">Ismail Halouani</h1>
+        <br/>
+        <img src="/Me-normal.jpg"/>
+        <br/>
+        <img src={imageInSrc}/>
+      </div>
+      <video width='320' height='240' controls>
+        <source src={myVideo} type="videp/mp4"/>
+      </video>
     </div>
   );
 }
